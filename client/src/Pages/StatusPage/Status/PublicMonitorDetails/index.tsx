a3503310c48e40d36950cdd8b51fdbbb4f0438ce
DevSam7t3 <samirashid54222@gmail.com>
2026-04-23 15:27:19 +0500

    refactor: remove unused useMemo import in PublicMonitorDetailsPage

--- client/src/Pages/StatusPage/Status/PublicMonitorDetails/index.tsx
+++ client/src/Pages/StatusPage/Status/PublicMonitorDetails/index.tsx
@@ -15,7 +15,7 @@ import {
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import dayjs from "dayjs";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
